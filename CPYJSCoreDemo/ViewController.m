db2ef3da93c40f3ac76f5ad7684ac77cbbc61a8b
Cielpy <beijiu572@gmail.com>
2016-06-19 00:15:05 +0800

    init

--- CPYJSCoreDemo/ViewController.m
+++ CPYJSCoreDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <JavaScriptCore/JavaScriptCore.h>
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@property (nonatomic, strong) JSContext *context;
 
 @end
 
@@ -17,6 +21,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"JSCoreDemo" ofType:@"html"];
+    
+    NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]];
+    NSString *htmlString = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    [self.webView loadHTMLString:htmlString baseURL:baseURL];
+    self.webView.delegate = self;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    JSContext *context = [self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    
+    [context setExceptionHandler:^(JSContext *ctx, JSValue *expectValue) {
+        NSLog(@"%@", expectValue);
+    }];
+    
+    self.context = context;
+    
+    __weak typeof(self) weakSelf = self;
+    self.context[@"ocAlert"] = ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            __strong typeof(weakSelf) strongSelf = weakSelf;
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message:@"这是OC中的弹框!" preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [alert dismissViewControllerAnimated:YES completion:^{
+                    
+                }];
+            }]];
+            [strongSelf.navigationController presentViewController:alert animated:YES completion:nil];
+        });
+    };
+}
+
+
+- (IBAction)buttonClick:(UIButton *)sender {
+    if (!self.context) {
+        return;
+    }
+    
+    JSValue *funcValue = self.context[@"alertFunc"];
+    [funcValue callWithArguments:nil];
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ README.md
@@ -0,0 +1,2 @@
+博客<<使用 JavaScriptCore 进行 JS和OC之间的通信>> 的demo
+https://imciel.com/2016/06/18/oc-js-communication/
