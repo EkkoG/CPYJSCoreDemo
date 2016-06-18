97f0812e0f31a725a0a4ff1b5393aa9600691567
Cielpy <beijiu572@gmail.com>
2016-06-18 13:39:56 +0800

    Initial Commit

---
+++ CPYJSCoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPYJSCoreDemo
+//
+//  Created by ciel on 16/6/18.
+//  Copyright © 2016年 CPY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CPYJSCoreDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CPYJSCoreDemo
+//
+//  Created by ciel on 16/6/18.
+//  Copyright © 2016年 CPY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
